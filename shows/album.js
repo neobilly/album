dd1f1beae8386ffb327bf24b832acaa561ca055d
neobilly <neob@hsh-global.com>
2012-09-27 13:26:10 +0200

    added a separate page to show albumview

---
+++ shows/album.js
@@ -0,0 +1,16 @@
+ /*show function serving up albumview pages, */
+function(doc, req) {
+  var html = "<div data-role=\"page\" id=\"albumview\">" +
+                       "<div data-role=\"header\" id=\"albumheader\">" +
+                           "<h2 class=\"albumtitle\">" + doc.title + "<\/h2>" +
+                       "<\/div>" +
+                       "<div data-role=\"content\" id=\"albumcontent\">" +
+                           "<h2 class=\"artist\">" + doc.artist + "<\/h2>" +
+                           "<p class=\"title\">" + doc.title + "<\/p>" +
+                           "<p class=\"description\">" + doc.description + "<\/p>" +
+                           "<p class=\"genre\">" + doc.genre + "<\/p>" +
+                       "<\/div>" +
+                       "<div data-role=\"footer\" \/>" +
+                   "<\/div>";
+  return html;
+}
