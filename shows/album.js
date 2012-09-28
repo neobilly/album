472d20221b717fca089f019816c415e602e05238
neobilly <neob@hsh-global.com>
2012-09-28 16:45:21 +0200

    made changes for the edit and delete pages

--- shows/album.js
+++ shows/album.js
@@ -1,16 +1,11 @@
- /*show function serving up albumview pages, */
-function(doc, req) {
-  var html = "<div data-role=\"page\" id=\"albumview\">" +
-                       "<div data-role=\"header\" id=\"albumheader\">" +
-                           "<h2 class=\"albumtitle\">" + doc.title + "<\/h2>" +
-                       "<\/div>" +
-                       "<div data-role=\"content\" id=\"albumcontent\">" +
-                           "<h2 class=\"artist\">" + doc.artist + "<\/h2>" +
-                           "<p class=\"title\">" + doc.title + "<\/p>" +
-                           "<p class=\"description\">" + doc.description + "<\/p>" +
-                           "<p class=\"genre\">" + doc.genre + "<\/p>" +
-                       "<\/div>" +
-                       "<div data-role=\"footer\" \/>" +
-                   "<\/div>";
-  return html;
+function(doc, req) { 
+	var Mustache = require("vendor/couchapp/lib/mustache");
+   	var stash = {
+			artist: doc.artist,
+			title : doc.title,
+			description: doc.description,
+			genre: doc.genre,
+			document: doc._id
+    };
+	return Mustache.to_html(this.templates.album, stash, this.templates.partials.album);
 }
--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -1,11 +1,5 @@
 function(doc) {
   if (doc.created_at) {
-      var p = doc.profile || {};
-      emit(doc.created_at, {
-          message:doc.message,
-          gravatar_url : p.gravatar_url,
-          nickname : p.nickname,
-          name : doc.name
-      });
+    emit(doc.created_at, doc);
   }
 };
